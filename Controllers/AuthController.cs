42ca48daea54133d199c99bd4b6e58bfae3166de
Balaji Naik <mvgnaik@gmail.com>
2020-03-12 18:03:37 +0530

    EndofSection5

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -47,7 +47,6 @@ namespace DatingApp.API.Controllers
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
 
-            throw new Exception("Computer says No!");
             var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
 
             if (userFromRepo == null)
