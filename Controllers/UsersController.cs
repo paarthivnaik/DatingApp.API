41136b503f10bd4281d44d1ef291bb413f9293fe
Balaji Naik <mvgnaik@gmail.com>
2020-05-06 22:05:36 +0530

    pagination,filtering and orderby

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -29,14 +29,21 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var userFromRepo = await _repo.GetUser(currentUserId);
+            userParams.UserId = currentUserId;
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+            }
             var users = await _repo.GetUsers(userParams);
 
             var userToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
-            Response.AddPagination(users.CurrentPage,users.PageSize,users.TotalCount,users.TotalPages);
+            Response.AddPagination(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
             return Ok(userToReturn);
         }
 
-        [HttpGet("{id}",Name="GetUser")]
+        [HttpGet("{id}", Name = "GetUser")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _repo.GetUser(id);
@@ -51,13 +58,13 @@ namespace DatingApp.API.Controllers
             if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
 
-                var userFromRepo= await _repo.GetUser(id);
-                _mapper.Map(userForUpdateDto,userFromRepo);
+            var userFromRepo = await _repo.GetUser(id);
+            _mapper.Map(userForUpdateDto, userFromRepo);
 
-                if(await _repo.SaveAll())
+            if (await _repo.SaveAll())
                 return NoContent();
 
-                throw new Exception($"Updating user {id} failed on save");
+            throw new Exception($"Updating user {id} failed on save");
         }
 
     }
